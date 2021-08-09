aaf90ebcdd4e6ba51c500a40452e5b5c40771e82
viniciusldn <viniciusldn0@gmail.com>
2021-08-09 18:19:32 -0300

    4.3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-Array-e-loop-for/for-of.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-Array-e-loop-for/for-of.js
@@ -1,5 +1,22 @@
-let names = ['vi', 'ju', 'ja', 'nu', 'la', 'ba', 'le'];
 
-for ( let nome of names){
-  console.log(nome);
+let lucknumbers = [];
+let mybet = [2 , 6 , 13 , 27, 60 ,38];
+let correct = [];
+let correctcount = 0;
+for (let count = 0; count < 6; count += 1) {
+  lucknumbers.push(Math.floor(Math.random()*60+1));
+}
+console.log(lucknumbers);
+
+for (let i = 0; i < lucknumbers.length; i += 1) {
+  if (lucknumbers.includes(mybet[i])) {
+    correct.push(mybet[i]);
+    correctcount += 1;
+  }
+}
+if (correct.length > 0) {
+  console.log("você acertou " + correctcount,": " + correct);
+}  
+else{
+  console.log("Você não acertou nada");
 }
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3/xadres-dama.js
@@ -0,0 +1,12 @@
+let n = 5;
+let square = [];
+
+if (n >= 1){
+  for (i = 0; i < n; i += 1){
+    square.push('*');
+  }
+  for (i = 0; i < n; i += 1){
+    console.log(square);
+  }
+  
+}

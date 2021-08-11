20627a4bdbe2420fb50120276b2cf2975292d5ca
viniciusldn <viniciusldn0@gmail.com>
2021-08-11 18:46:06 -0300

    Q1 Answered

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,16 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+// Q1
+function createDay() {
+  let day;
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    day = document.createElement('li');
+    day.innerText = dezDaysList[index];
+    day.className = "day";
+    document.getElementById('days').appendChild(day);
+  }
+}
+
+createDay();

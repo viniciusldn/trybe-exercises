fb8e3cdd587bf30e48c75f27f8362de6c382e8cb
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-02-08 19:17:18 -0300

    R05 DONE

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/22.1/imc.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/22.1/imc.js
@@ -5,7 +5,13 @@ const ALTURA = rls.questionFloat('Qual sua altura em metros?\n');
 
 const computeImc = (weight, height) => {
   const imc = (weight / Math.pow(height, 2).toFixed(2));
-  console.log(`IMC: ${imc}`)
+
+  if (imc < 18.5) console.log(`IMC: ${imc} - Abaixo do peso (magreza)`);
+  else if (imc >= 18.5 && imc < 25) console.log(`IMC: ${imc} - Peso normal`);
+  else if (imc >= 25 && imc < 30) console.log(`IMC: ${imc} - Acima do peso (sobrepeso)`);
+  else if (imc >= 30 && imc < 35) console.log(`IMC: ${imc} - Obesidade graus I`);
+  else if (imc >= 35 && imc < 40) console.log(`IMC: ${imc} - Obesidade graus II`);
+  else console.log(`IMC: ${imc} - Obesidade graus III e IV`);
 };
 
 computeImc(PESO,ALTURA);

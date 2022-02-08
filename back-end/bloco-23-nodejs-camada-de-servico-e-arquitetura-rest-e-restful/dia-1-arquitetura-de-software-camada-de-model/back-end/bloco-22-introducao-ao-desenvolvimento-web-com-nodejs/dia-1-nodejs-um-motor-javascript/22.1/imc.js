1a22482e1bad5c083fe6146a63aa7f5656313feb
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-02-08 18:57:03 -0300

    npm i readline-sync & R03 DONE

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/22.1/imc.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-1-arquitetura-de-software-camada-de-model/back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/22.1/imc.js
@@ -1,5 +1,7 @@
-const PESO = 83;
-const ALTURA = 1.79;
+const rls = require('readline-sync');
+
+const PESO = rls.questionFloat('Qual seu peso em kilos?\n');
+const ALTURA = rls.questionFloat('Qual sua altura em metros?\n');
 
 const computeImc = (weight, height) => {
   const imc = (weight / Math.pow(height, 2).toFixed(2));

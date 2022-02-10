8401c5d133bc5f8908cb952889349660a82b3750
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-02-10 10:49:13 -0300

    R02 done

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio1.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio1.js
@@ -1,18 +1,29 @@
 const readline = require('readline-sync');
 
-function main() {
-  const param1 = readline.questionInt('Type a number\n');
-  const param2 = readline.questionInt('Type a number to add\n');
-  const param3 = readline.questionInt('Type a number to multiply\n');
-  if (isNaN(param1)||isNaN(param2)||isNaN(param2)) throw new Error('Input is not a number')
+function checkType(...el) {
+  // const check = (element) => isNaN(element);
+  const isNum = [...el].some(element => isNaN(element));
+  return isNum;
+}
+
+function main(a,b,c) {
+  let param1 = a;
+  let param2 = b;
+  let param3 = c;
+  if (!a,!b,!c) {
+    param1 = readline.questionInt('Type a number\n', { limitMessage: 'Input is not a number'});
+    param2 = readline.questionInt('Type a number to add\n', { limitMessage: 'Input is not a number'});
+    param3 = readline.questionInt('Type a number to multiply\n', { limitMessage: 'Input is not a number'});
+  }
   const result = ( (param1 + param2) * param3 );
   const promise = new Promise ((resolve, reject) => {
-    if (result >= 50) resolve (`\nThe result is ${result}\n`)
-    reject ('Result value is too low')
+    if (result >= 50) resolve (`\nThe result is ${result}\n`);
+    else if (checkType(a,b,c)){
+      reject ('Input only numbers');
+    };
+    reject ('Result value is too low');
   })
   return promise;
 }
 
-main()
-  .then((resolve) => console.log(resolve))
-  .catch((reject) => console.log(reject));
+module.exports = main;

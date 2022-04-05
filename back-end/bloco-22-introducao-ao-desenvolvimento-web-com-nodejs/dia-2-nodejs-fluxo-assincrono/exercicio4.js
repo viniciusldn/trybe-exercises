9dcd9b49ef529adfd0c3ab9dfb1a060b09d84945
Vinicius Leite Dourado Nogueira <viniciusldn0@gmail.com>
2022-04-05 16:51:06 -0300

    aula 22.2

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio4.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/exercicio4.js
@@ -1,11 +1,32 @@
+const { resolve } = require('path');
+
 const fs = require('fs').promises;
 
 fs.readFile('./simpsons.json', 'utf-8')
   .then((fileContent) => JSON.parse(fileContent))
   .then((simpsons) => listSimpsons(simpsons))
 
-function listSimpsons(p) {
-  for (index = 0; index < p.length; index += 1) {
-    console.log(`${p[index].id} - ${p[index].name}`);
+function listSimpsons(arr) {
+  for (index = 0; index < arr.length; index += 1) {
+    console.log(`${arr[index].id} - ${arr[index].name}`);
   }
 }
+
+async function searchId(id) {
+  const simpsonsJSON = await fs.readFile('./simpsons.json', 'utf-8')
+  return new Promise((resolve, reject) => {
+    const simpsonsArr = JSON.parse(simpsonsJSON);
+    const simpson = simpsonsArr.find((el) => Number(el.id) === Number(id));
+    if (simpson) resolve (simpson);
+    reject ('ID NOT FOUND');
+    // Não consegui fazer funcionar com o for
+    /* for (index = 0; index < simpsonsArr.length; index += 1) {
+      if (simpsonsArr[index].id === id) resolve (simpsonsArr[index])
+      reject ('ID NOT FOUND');
+    } */
+  })
+}
+
+searchId(10)
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
